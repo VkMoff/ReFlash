6c30fad4139d819dfe2de230beb3754af08bf48f
VkMoff <vitekmartynov@yandex.ru>
2026-05-17 22:44:13 +0300

    Description fixes

--- scripts/resources/RandomEncounterResource.cs
+++ scripts/resources/RandomEncounterResource.cs
@@ -6,4 +6,5 @@ using System;
 public partial class RandomEncounterResource : EncounterResource
 {
     [Export] public PackedScene Scene;
+    public override string GetDescription() {return "[font=res://resources/styles/Play-Regular.ttf][font_size=48][outline_size=2]Случайное событие";}
 }
--- scripts/resources/ShopResource.cs
+++ scripts/resources/ShopResource.cs
@@ -3,5 +3,5 @@ using Godot;
 [GlobalClass]
 public partial class ShopResource : EncounterResource
 {
-    
+    public override string GetDescription() {return "[font=res://resources/styles/Play-Regular.ttf][font_size=48][outline_size=2]Магазин [color=F9C435]артефактов";}
 }
